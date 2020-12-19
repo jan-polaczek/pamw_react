9953bb0ee786cd4a87f0b86b302a3822af8167cf
jan-polaczek <48283663+jan-polaczek@users.noreply.github.com>
2020-12-19 22:15:53 +0100

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export const urls = {
-    apiPath : 'https://localhost:5000',
+    apiPath : 'https://jan-polaczek-pamw.herokuapp.com',
     packagesPath : '/api/packages/',
     sendersPath : '/api/senders/',
     authPath: '/api/auth/'
